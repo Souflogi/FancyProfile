c3f79e8db42788988cac4229d9df92a5351b5593
Soufiane <sammahsoufiane01@gmail.com>
2022-12-24 19:47:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// const AllpersonEl = document.querySelectorAll(".person");
+
+// const descripEl = document.querySelector(".description");
+
+// AllpersonEl.forEach(person => {
+//   person.addEventListener("mouseover", e => {
+//     e.currentTarget.nextElementSibling.classList.toggle("hidden");
+//   });
+// });
+
+// AllpersonEl.forEach(person => {
+//   person.addEventListener("mouseleave", e => {
+//     e.currentTarget.nextElementSibling.classList.toggle("hidden");
+//   });
+// });
